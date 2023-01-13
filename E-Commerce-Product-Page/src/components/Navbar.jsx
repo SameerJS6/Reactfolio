7668fbff1e2c668047ff5611e9a3310964b54e8e
Sameer Singh <sameersingh1234pok@gmail.com>
2023-01-14 00:02:01 +0530

    Navbar Complete

--- E-Commerce-Product-Page/src/components/Navbar.jsx
+++ E-Commerce-Product-Page/src/components/Navbar.jsx
@@ -6,17 +6,18 @@ import ProfileImage from '../assets/image-avatar.png'
 
 export default function Navbar() {
  const [isOpen, setIsOpen] = useState(false); 
-
+   isOpen ? document.body.classList.add("overlay") : document.body.classList.remove("overlay");
+   
   return (
    <>
-     <header>
+     <header data-overlay={`${isOpen ? "true" : "false"}`} >
       <div className="logo">
       <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
         {!isOpen && <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
           <path d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z" fill="#69707D" fillRule="evenodd"/>
         </svg>}
 
-        {isOpen && <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+        {isOpen && <svg className="close-svg" width="16" height="15" xmlns="http://www.w3.org/2000/svg">
           <path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fillRule="evenodd"/>
         </svg>}
 
