88db4c47199ea1432a3c4ae0d26a66c018d18354
Sameer Singh <sameersingh1234pok@gmail.com>
2023-01-15 21:37:08 +0530

    Image Slider and Image LightBox Added

--- E-Commerce-Product-Page/src/components/ImageSlider.jsx
+++ E-Commerce-Product-Page/src/components/ImageSlider.jsx
@@ -6,30 +6,9 @@ import image3 from '../assets/image-product-3.jpg'
 import image4 from '../assets/image-product-4.jpg'
 
 export default function ImageSlider() {
-  const Images = [
-    {
-      id: 1,
-      src: image,
-      caption: "Main Image"
-    },
-    {
-      id: 2,
-      src: image2,
-      caption: "Image2"
-    },
-    {
-      id: 3,
-      src: image3,
-      caption: "Image3"
-    },
-    {
-      id: 4,
-      src: image4,
-      caption: "Image4"
-    }
-  ]
-
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const Images = [ image, image2, image3, image4 ]
 
   const handlePrevClick = () => {
     const prevIndex = currentIndex - 1;
@@ -41,31 +20,61 @@ export default function ImageSlider() {
     console.log("Next Button Was Clicked");
     setCurrentIndex(nextIndex === Images.length ? 0 : nextIndex);
   }
+  const handleClick = (index) => {
+    setCurrentIndex(index)
+  }
   return (
     <>
-      <section className="wrapper">
+      <section className="wrapper" data-active="false">  
         <div className="main-img">
-          <img className="hero-img" src={Images[currentIndex].src} alt={Images[currentIndex].caption} />
+          <img className="hero-img" src={Images[currentIndex]} alt={Images[currentIndex].caption} onClick={()=> setIsOpen(!isOpen)}/>
 
           <button className="arrows left-arrow" onClick={ handlePrevClick }>
-          <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11 1 3 9l8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/>
-          </svg>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11 1 3 9l8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/>
+            </svg>
           </button>
 
           <button className="arrows right-arrow" onClick={ handleNextClick }>
-          <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-            <path d="m2 1 8 8-8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/>
-          </svg>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path d="m2 1 8 8-8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/>
+            </svg>
           </button>
         </div>
           <div className="image-wrapper">
-            <div className="wrapper-content active-img"><img className="imgCarousel" src={image} alt="MainImage" /></div>
-            <div className="wrapper-content"><img className="imgCarousel" src={image2} alt="Image2" /></div>
-            <div className="wrapper-content"><img className="imgCarousel" src={image3} alt="Image3" /></div>
-            <div className="wrapper-content"><img className="imgCarousel" src={image4} alt="Image4" /></div>
-          </div>
+            {Images.map((image, index) => (
+              <div key={index} className={`wrapper-content ${currentIndex === index ? "active-img" : ''}`} onClick={() => handleClick(index)}>
+                <img className="imgCarousel" src={image} alt="MainImage" />
+              </div>
+            ))}
+          </div> 
       </section>
+
+      {isOpen && <section className="lightbox opacity" data-overlay={`${!isOpen ? "true" : "false"}`}>
+            <button className='lightbox-close-btn' onClick={()=> setIsOpen(!isOpen)}>X</button>
+          <div className="main-img translate">
+            <img className="hero-img" src={Images[currentIndex]} alt={Images[currentIndex].caption}  />
+          </div>
+            <button className="box arrows left-arrow" onClick={ handlePrevClick }>
+              <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11 1 3 9l8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/>
+              </svg>
+            </button>
+            <button className="box arrows right-arrow" onClick={ handleNextClick }>
+              <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+                <path d="m2 1 8 8-8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/>
+              </svg>
+            </button>
+        
+
+          <div className="image-wrapper">
+            {Images.map((image, index) => (
+              <div key={index} className={`wrapper-content ${currentIndex === index ? "active-img" : ''}`} onClick={() => handleClick(index)}>
+                <img className="imgCarousel" src={image} alt="MainImage" />
+              </div>
+            ))}
+          </div> 
+      </section> }
     </>
   )
 }
