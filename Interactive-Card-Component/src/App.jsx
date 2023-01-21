428e858b792f3af1a5cb5ad1c5d0694c8b01bc4c
Sameer Singh <sameersingh1234pok@gmail.com>
2023-01-21 23:24:48 +0530

    File Commited

---
+++ Interactive-Card-Component/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-center text-white font-bold grid min-h-screen place-content-center bg-slate-900'>
+      App
+    </div>
+  )
+}
---
+++ Interactive-Card-Component/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
