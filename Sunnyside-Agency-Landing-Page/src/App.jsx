7c73b6a839f09d9959e6a2cbde352e122ff89284
Sameer Singh <sameersingh1234pok@gmail.com>
2023-01-29 22:53:01 +0530

    Project Commited

---
+++ Sunnyside-Agency-Landing-Page/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <h1 className='grid min-h-screen font-bold place-content-center text-center text-lime-400 rounded-md bg-black p-4 text-3xl'>App</h1>
+    </>
+  )
+}
---
+++ Sunnyside-Agency-Landing-Page/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
