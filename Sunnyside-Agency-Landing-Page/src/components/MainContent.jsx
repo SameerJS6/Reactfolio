1599f4c28e1fb7cbead615db8edf3228d8f6d3cf
Sameer Singh <sameersingh1234pok@gmail.com>
2023-01-30 23:28:13 +0530

    Basic Layout

---
+++ Sunnyside-Agency-Landing-Page/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile1 from '../assets/image-emily.jpg'
+import Profile2 from '../assets/image-jennie.jpg'
+import Profile3 from '../assets/image-thomas.jpg'
+
+export default function MainContent() {
+  return (
+    <>
+        <main className='min-h-screen'>
+            <h1>Main Content</h1>
+        </main>
+    </>
+  )
+}
