5e9bf9f4c993887c713c0695f96a8423493cce26
Sameer Singh <sameersingh1234pok@gmail.com>
2023-02-01 21:16:51 +0530

    Navbar Done

--- Sunnyside-Agency-Landing-Page/src/App.jsx
+++ Sunnyside-Agency-Landing-Page/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MainContent from './components/MainContent';
 import Navbar from './components/Navbar';
+import MainContent from './components/MainContent';
 
 export default function App() {
   return (
