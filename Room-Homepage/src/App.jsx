d06a568291fba0842f95ee116197a3f33a9563fe
Sameer Singh <sameersingh1234pok@gmail.com>
2023-02-07 09:53:17 +0530

    5th Project Commited

---
+++ Room-Homepage/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+    <h1 className='text-center text-lime-400 text-2xl md:text-4xl font-extrabold'>Hello, I am building this Frontend Mentor Project using React JS and GSAP</h1>
+    </>
+  )
+}
---
+++ Room-Homepage/src/components/FooterContent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FooterContent() {
+  return (
+    <>FooterContent</>
+  )
+}
