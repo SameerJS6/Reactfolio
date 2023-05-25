57a56d57e585c4fe8d3d055bbf4b0c4804515605
SameerJS6 <sameersingh1234pok@gmail.com>
2023-05-25 21:31:23 +0530

    Functinality Added

--- Age-Calculator/src/App.jsx
+++ Age-Calculator/src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [age, setAge] = useState({});
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const today = new Date(); //This line creates a new Date() object representing the current date and time.
+    const birthDate = new Date(`${month}/${day}/${year}`); //This line creates a new Date() object representing the birth date using the user input values.
+    const timeDiff = today.getTime() - birthDate.getTime(); //This line calculates the time difference between the current date and the user input date in milliseconds.
+    const ageDate = new Date(timeDiff); //This line creates a new Date using the time calculated in the previous step.
+    const years = Math.abs(ageDate.getUTCFullYear() - 1970); //This line calculates the number of years between birth year and current year.
+    const months = ageDate.getUTCMonth(); //This line gets the birth month.
+    const days = ageDate.getUTCDate() - 1; //This line get the birth date, and subtracting 1 from the result adjust the fact that ageDate represents a duration and not a specific date.
+
+    setAge({ year: years, month: months, day: days });
+    setDay("");
+    setMonth("");
+    setYear("");
   };
   return (
     <>
@@ -17,32 +35,54 @@ export default function App() {
               <div className="grid grid-cols-3 gap-4 md:w-10/12 md:gap-6">
                 <div className="relative">
                   <input
-                    className="peer w-full rounded-lg border-[1.5px] border-[#dbdbdb] p-3 text-2xl font-extrabold text-[#141414] sm:text-3xl md:px-4"
+                    className="peer w-full rounded-lg border-[1.5px] border-[#dbdbdb] p-3 text-2xl font-extrabold text-[#141414] transition-all duration-200 ease-in-out sm:text-3xl md:px-4"
                     type="number"
                     name="day"
                     id="day"
                     placeholder=" "
+                    value={day}
+                    onChange={(e) => setDay(e.target.value)}
                   />
+                  {/* Tempo Label for very small screen devices  */}
                   <label
-                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 ease-in-out peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#716f6f] peer-focus-visible:-top-2 peer-focus-visible:left-3 peer-focus-visible:text-xs peer-focus-visible:text-[#854dff] max-[350px]:peer-placeholder-shown:text-base max-[350px]:peer-focus-visible:text-xs max-[300px]:peer-placeholder-shown:text-base max-[300px]:peer-focus-visible:-top-1.5 max-[300px]:peer-focus-visible:text-xs sm:peer-placeholder-shown:top-3 sm:peer-placeholder-shown:text-3xl sm:peer-focus-visible:-top-2 sm:peer-focus-visible:text-xs md:left-4 md:text-sm md:peer-placeholder-shown:left-3 md:peer-placeholder-shown:top-4 md:peer-placeholder-shown:text-2xl md:peer-focus-visible:-top-2 md:peer-focus-visible:text-xs"
+                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 peer-focus-visible:text-[#854dff] min-[350px]:hidden"
                     htmlFor="day"
                   >
                     Day
                   </label>
-                  <p className="translate-y-2 text-[10px] italic text-[#ff5757] opacity-100 transition-all duration-300 sm:text-xs">
+                  {/* Below the Floating Label for Tablets  */}
+                  <label
+                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 ease-in-out peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#716f6f] peer-focus-visible:-top-2 peer-focus-visible:left-3 peer-focus-visible:text-xs peer-focus-visible:text-[#854dff] max-[350px]:hidden sm:peer-placeholder-shown:top-3 sm:peer-placeholder-shown:text-3xl sm:peer-focus-visible:-top-2 sm:peer-focus-visible:text-xs md:left-4 md:text-xs md:peer-placeholder-shown:left-3 md:peer-placeholder-shown:top-4 md:peer-placeholder-shown:text-2xl md:peer-focus-visible:-top-2 md:peer-focus-visible:text-xs"
+                    htmlFor="day"
+                  >
+                    Day
+                  </label>
+                  <p
+                    className={`translate-y-2 text-[10px] italic text-[#ff5757] transition-all duration-300 sm:text-xs `}
+                  >
                     The field is required
                   </p>
                 </div>
                 <div className=" relative">
                   <input
-                    className="peer w-full rounded-lg border-[1.5px] border-[#dbdbdb] p-3 text-2xl font-extrabold text-[#141414] md:px-4 md:text-3xl"
+                    className="peer w-full rounded-lg border-[1.5px] border-[#dbdbdb] p-3 text-2xl font-extrabold text-[#141414] transition-all duration-200 ease-in-out md:px-4 md:text-3xl"
                     type="number"
                     name="month"
                     id="month"
                     placeholder=" "
-                  />
+                    value={month}
+                    onChange={(e) => setMonth(e.target.value)}
+                  />{" "}
+                  {/* Tempo Label for very small screen devices  */}
                   <label
-                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 ease-in-out peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#716f6f] peer-focus-visible:-top-2 peer-focus-visible:left-3 peer-focus-visible:text-xs peer-focus-visible:text-[#854dff] max-[350px]:peer-placeholder-shown:text-base max-[350px]:peer-focus-visible:text-xs max-[300px]:peer-placeholder-shown:text-base max-[300px]:peer-focus-visible:-top-1.5 max-[300px]:peer-focus-visible:text-xs sm:peer-placeholder-shown:top-3 sm:peer-placeholder-shown:text-3xl sm:peer-focus-visible:-top-2 sm:peer-focus-visible:text-xs md:left-4 md:text-sm md:peer-placeholder-shown:left-3 md:peer-placeholder-shown:top-4 md:peer-placeholder-shown:text-2xl md:peer-focus-visible:-top-2 md:peer-focus-visible:text-xs"
+                    className="pointer-events-none absolute -top-2 left-2 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 peer-focus-visible:text-[#854dff] min-[350px]:hidden"
+                    htmlFor="month"
+                  >
+                    Month
+                  </label>
+                  {/* Below the Floating Label for Tablets  */}
+                  <label
+                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 ease-in-out peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#716f6f] peer-focus-visible:-top-2 peer-focus-visible:left-3 peer-focus-visible:text-xs peer-focus-visible:text-[#854dff] max-[350px]:hidden sm:peer-placeholder-shown:top-3 sm:peer-placeholder-shown:text-3xl sm:peer-focus-visible:-top-2 sm:peer-focus-visible:text-xs md:left-4 md:text-xs md:peer-placeholder-shown:left-3 md:peer-placeholder-shown:top-4 md:peer-placeholder-shown:text-2xl md:peer-focus-visible:-top-2 md:peer-focus-visible:text-xs"
                     htmlFor="month"
                   >
                     Month
@@ -53,14 +93,24 @@ export default function App() {
                 </div>
                 <div className="relative">
                   <input
-                    className="peer w-full rounded-lg border-[1.5px] border-[#dbdbdb] p-3 text-2xl font-extrabold text-[#141414] md:px-4 md:text-3xl"
+                    className="peer w-full rounded-lg border-[1.5px] border-[#dbdbdb] p-3 text-2xl font-extrabold text-[#141414] transition-all duration-200 ease-in-out md:px-4 md:text-3xl"
                     type="number"
                     name="year"
                     id="year"
                     placeholder=" "
-                  />
+                    value={year}
+                    onChange={(e) => setYear(e.target.value)}
+                  />{" "}
+                  {/* Tempo Label for very small screen devices  */}
                   <label
-                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 ease-in-out peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#716f6f] peer-focus-visible:-top-2 peer-focus-visible:left-3 peer-focus-visible:text-xs peer-focus-visible:text-[#854dff] max-[350px]:peer-placeholder-shown:text-base max-[350px]:peer-focus-visible:text-xs max-[300px]:peer-placeholder-shown:text-base max-[300px]:peer-focus-visible:-top-1.5 max-[300px]:peer-focus-visible:text-xs sm:peer-placeholder-shown:top-3 sm:peer-placeholder-shown:text-3xl sm:peer-focus-visible:-top-2 sm:peer-focus-visible:text-xs md:left-4 md:text-sm md:peer-placeholder-shown:left-3 md:peer-placeholder-shown:top-4 md:peer-placeholder-shown:text-2xl md:peer-focus-visible:-top-2 md:peer-focus-visible:text-xs"
+                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 peer-focus-visible:text-[#854dff] min-[350px]:hidden"
+                    htmlFor="year"
+                  >
+                    Year
+                  </label>
+                  {/* Below the Floating Label for Tablets  */}
+                  <label
+                    className="pointer-events-none absolute -top-2 left-3 bg-white text-xs font-bold uppercase tracking-widest text-[#716f6f] transition-all duration-300 ease-in-out peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#716f6f] peer-focus-visible:-top-2 peer-focus-visible:left-3 peer-focus-visible:text-xs peer-focus-visible:text-[#854dff] max-[350px]:hidden sm:peer-placeholder-shown:top-3 sm:peer-placeholder-shown:text-3xl sm:peer-focus-visible:-top-2 sm:peer-focus-visible:text-xs md:left-4 md:text-xs md:peer-placeholder-shown:left-3 md:peer-placeholder-shown:top-4 md:peer-placeholder-shown:text-2xl md:peer-focus-visible:-top-2 md:peer-focus-visible:text-xs"
                     htmlFor="year"
                   >
                     Year
@@ -83,7 +133,7 @@ export default function App() {
                     viewBox="0 0 46 44"
                     className="transition-all duration-300 ease-in-out group-hover:translate-y-1 group-hover:scale-90"
                   >
-                    <g fill="none" stroke="#fff" stroke-width="2">
+                    <g fill="none" stroke="#fff" strokeWidth="2">
                       <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
                     </g>
                   </svg>
@@ -94,13 +144,22 @@ export default function App() {
           {/* The Output Section  */}
           <div className="grid gap-1 text-5xl font-extrabold italic text-[#141414] sm:text-6xl md:text-[4rem] lg:text-7xl">
             <h1 className="">
-              <span className="text-[#854dff]">--</span> years
+              <span className="text-[#854dff]">
+                {age.year ? age.year : "--"}
+              </span>{" "}
+              years
             </h1>
             <h2 className="">
-              <span className="text-[#854dff]">--</span> months
+              <span className="text-[#854dff]">
+                {age.month ? age.month : "--"}
+              </span>{" "}
+              months
             </h2>
             <h3 className="">
-              <span className="text-[#854dff]">--</span> days
+              <span className="text-[#854dff]">
+                {age.day >= 0 ? age.day : "--"}
+              </span>{" "}
+              days
             </h3>
           </div>
         </div>
