ba18b679306392c6a05bfc393215d2f503a9c50d
SameerJS6 <sameersingh1234pok@gmail.com>
2023-05-27 21:33:54 +0530

    Hero Section Added

--- All-Project/src/App.jsx
+++ All-Project/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <>
-      <Navbar/>
+      {/* <Navbar /> */}
+      <Hero />
     </>
-  )
+  );
 }
--- All-Project/src/components/Cards.jsx
+++ All-Project/src/components/Cards.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Cards() {
   return (
-    <div>Cards</div>
+    <>Cards</>
   )
 }
