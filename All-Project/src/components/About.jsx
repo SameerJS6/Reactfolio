c60f4bcf2626b51c4d0f125e27c7cf6847e78049
SameerJS6 <sameersingh1234pok@gmail.com>
2023-05-29 00:17:06 +0530

    About Me Section Added

--- All-Project/src/components/About.jsx
+++ All-Project/src/components/About.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <section className="mx-6 lg:mx-12" id="About">
+      <div className="about-gradient my-4 mt-16 grid min-h-[300px] rounded-3xl px-4 py-2 sm:my-10 sm:mt-20 sm:rounded-[2.5rem] sm:py-2 md:my-[3.5rem] md:mt-20 md:rounded-[3rem] md:py-2 lg:my-20 lg:mt-28 lg:min-h-[450px] lg:py-4">
+        <div className="grid place-content-center items-center gap-2 px-2 py-12 text-left sm:gap-4 sm:px-8 xl:place-content-start xl:py-24">
+          <h2
+            className="text-[50px] sm:text-6xl md:text-[77px] lg:text-[80px] xl:text-[112px] xl:leading-snug"
+            role="heading"
+          >
+            About
+          </h2>
+          <p
+            className=" max-w-[60ch] text-base sm:text-lg md:text-xl lg:text-[22px]"
+            role="Sub Heading"
+          >
+            Reactfolio, where you can explore my diverse range of
+            self-developed React projects. Discover innovative web applications
+            that demonstrate the power and versatility of React.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
