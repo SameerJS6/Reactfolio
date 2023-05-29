f0db92d9c5c82c623547bc5f34b964614bffcd04
SameerJS6 <sameersingh1234pok@gmail.com>
2023-05-29 20:48:17 +0530

    Minor Changes

--- All-Project/src/App.jsx
+++ All-Project/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <>
       <div className="mx-auto max-w-[1780px]">
-        {/* <Navbar /> */}
         <Hero />
         <Footer />
         <ScrollToTop />
