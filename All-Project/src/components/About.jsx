2fb63cb40dc00c75a88b3dab2b9419653fa55191
SameerJS6 <sameersingh1234pok@gmail.com>
2023-06-01 22:02:06 +0530

    Scrolling Adjusted

--- All-Project/src/components/About.jsx
+++ All-Project/src/components/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="mx-6 lg:mx-12" id="About">
-      <div className="about-gradient my-4 mt-16 grid min-h-[300px] rounded-3xl px-4 py-2 sm:my-10 sm:mt-20 sm:rounded-[2.5rem] sm:py-2 md:my-[3.5rem] md:mt-20 md:rounded-[3rem] md:py-2 lg:my-20 lg:mt-28 lg:min-h-[450px] lg:py-4">
+    <section className="mx-6 lg:mx-12 py-12 pb-4 lg:py-8 lg:pb-2" id="About">
+      <div className="about-gradient my-4 mt-8 mb-4 grid min-h-[300px] rounded-3xl px-4 py-2 sm:my-10 sm:mt-20 sm:rounded-[2.5rem] sm:py-2  md:rounded-[3rem] lg:my-20 lg:mt-16 lg:min-h-[450px] lg:py-4">
         <div className="grid place-content-center items-center gap-2 px-2 py-12 text-left sm:gap-4 sm:px-8 xl:place-content-start xl:py-24">
           <h2
             className="text-[50px] sm:text-6xl md:text-[77px] lg:text-[80px] xl:text-[112px] xl:leading-snug"
