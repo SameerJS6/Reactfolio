d9ece2debdc10a3032d4458ca24d3e3230b45927
SameerJS6 <sameersingh1234pok@gmail.com>
2023-06-04 23:08:30 +0530

    GSAP Animations Added

--- All-Project/src/App.jsx
+++ All-Project/src/App.jsx
@@ -1,13 +1,109 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import { gsap } from "gsap";
+import SplitType from "split-type";
+import debounce from "lodash.debounce";
+import ResizeObserver from "resize-observer-polyfill";
 
 export default function App() {
+  const textRef = useRef(null);
+  const containerRef = useRef(null);
+  const resizeObserverRef = useRef(null);
+
+  var tl = gsap.timeline();
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      const containerWidth = containerRef.current.offsetWidth;
+
+      if (
+        textRef.current &&
+        containerWidth !== textRef.current.containerWidth
+      ) {
+        textRef.current.containerWidth = containerWidth;
+        textRef.current.split();
+      }
+    }, 900);
+
+    resizeObserverRef.current = new ResizeObserver(handleResize);
+    resizeObserverRef.current.observe(containerRef.current);
+
+    return () => resizeObserverRef.current.disconnect();
+    // console.log(containerRef.current.offsetWidth);
+  }, []);
+
+  useEffect(() => {
+    const textElement = document.querySelector("#target");
+    textRef.current = SplitType.create(textElement);
+    textRef.current.containerWidth = containerRef.current.offsetWidth;
+    // let text = SplitType.create("#target");
+
+    gsap.to(".mainApp", { duration: 0, css: { visibility: "visible" } });
+    tl.from(".heroNav", {
+      duration: 1,
+      y: -25,
+      opacity: 0,
+      ease: "back.out",
+      delay: 0.25,
+      stagger: 0.1,
+    })
+      .from(textRef.current.chars, {
+        duration: 0.9,
+        yPercent: 130,
+        opacity: 0,
+        ease: "back.out",
+        stagger: 0.05,
+        delay: -1.4,
+      })
+      .fromTo(
+        ".heroText",
+        {
+          y: 20,
+          opacity: 0,
+        },
+        {
+          duration: 1,
+          y: 0,
+          opacity: 1,
+          ease: "power3.out",
+          delay: -0.9,
+        }
+      )
+      .fromTo(
+        ".heroBtn",
+        {
+          y: 20,
+          opacity: 0,
+        },
+        {
+          duration: 1,
+          y: 0,
+          opacity: 1,
+          ease: "power3.out",
+          delay: -0.9,
+        }
+      )
+      .fromTo(
+        ".projectText",
+        {
+          y: 20,
+          opacity: 0,
+        },
+        {
+          duration: 1,
+          y: 0,
+          opacity: 1,
+          ease: "power3.out",
+          delay: -0.9,
+        }
+      )
+  }, []);
   return (
     <>
-      <div className="mx-auto max-w-[1780px]">
-        <Hero />
+      <div className="mainApp mx-auto max-w-[1780px]">
+        <Hero containerRef={containerRef} />
         <Footer />
         <ScrollToTop />
       </div>
--- All-Project/src/components/Hero.jsx
+++ All-Project/src/components/Hero.jsx
@@ -4,36 +4,44 @@ import Cards from "./Cards";
 import About from "./About";
 import Ripple from "../hook/Ripple";
 
-export default function Hero() {
+export default function Hero({ containerRef }) {
   return (
     <>
       <main>
-        <div className="hero-gradient min-h-[410px] rounded-b-3xl sm:rounded-b-[2.5rem] md:rounded-b-[3rem] lg:min-h-[450px] xl:rounded-b-[4rem] text-onBackground">
+        <div
+          ref={containerRef}
+          className="hero-gradient min-h-[410px] rounded-b-3xl text-onBackground sm:rounded-b-[2.5rem] md:rounded-b-[3rem] lg:min-h-[450px] xl:rounded-b-[4rem]"
+        >
           <Navbar />
 
-          <div className="grid place-content-center gap-4 px-4 py-12 text-center sm:px-8 md:px-10 xl:py-24 transition-colors duration-300">
+          <div className="grid place-content-center gap-4 px-4 py-12 text-center transition-colors duration-300 sm:px-8 md:px-10 xl:py-24">
             <h1
               className="text-[50px] font-medium leading-snug sm:text-6xl md:text-[80px] xl:text-[112px]"
+              id="target"
               role="heading"
             >
               Learning by Building
             </h1>
-            <p
-              className="mx-auto max-w-[85ch] py-4 text-base sm:text-lg md:text-[22px]"
-            >
+            <p className="heroText mx-auto max-w-[85ch] py-4 text-base sm:text-lg md:text-[22px]">
               Showcasing My Impressive Collection of Self-Developed React
               Projects, Browse My React Projects: Experience clean and
               impressive creations firsthand.
             </p>
-            <a
-              href="#Projects"
-              type="button"
-              aria-label="Get Started"
-              className="relative mx-auto w-fit overflow-hidden rounded-[48px] bg-primary px-6 py-4 text-xl font-medium text-onPrimary transition-all duration-300 hover:bg-opacity-[95%] active:rounded-3xl md:px-8 md:py-6 md:text-2xl"
-            >
-              Explore My Projects
-              <Ripple color="rgb(var(--on-surface))" opacity={0.2} duration={600} />
-            </a>
+            <div className="heroBtn">
+              <a
+                href="#Projects"
+                type="button"
+                aria-label="Get Started"
+                className="relative mx-auto w-fit overflow-hidden rounded-[48px] bg-primary px-6 py-4 text-xl font-medium text-onPrimary transition-all duration-300 hover:bg-opacity-[95%] active:rounded-3xl md:px-8 md:py-6 md:text-2xl"
+              >
+                Explore My Projects
+                <Ripple
+                  color="rgb(var(--on-surface))"
+                  opacity={0.2}
+                  duration={600}
+                />
+              </a>
+            </div>
           </div>
         </div>
 
