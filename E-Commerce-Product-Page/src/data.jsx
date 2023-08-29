7363b7044b4badf5eeb45318e5c0fa0678baebca
Sameer Singh <sameersingh1234pok@gmail.com>
2023-08-29 20:15:14 +0530

    Data Added

---
+++ E-Commerce-Product-Page/src/data.jsx
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 1,
+    companyName: "SNEAKER COMPANY",
+    productTitle: "fall limited edition sneakers",
+    description:
+      "These low-profile sneakersare your perfect casual werar companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+    price: 125.00,
+    discount: "50%",
+    actualPrice: 250.00
+  },
+];
