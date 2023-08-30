dc76f10807dd042bf2a2246e376e251f2d9e11da
Sameer Singh <sameersingh1234pok@gmail.com>
2023-08-30 20:03:57 +0530

    Data Updated

--- E-Commerce-Product-Page/src/data.jsx
+++ E-Commerce-Product-Page/src/data.jsx
@@ -1,12 +1,21 @@
-export default [
+import image from "./assets/image-product-1.jpg";
+import image2 from "./assets/image-product-2.jpg";
+import image3 from "./assets/image-product-3.jpg";
+import image4 from "./assets/image-product-4.jpg";
+
+const Item = [
   {
     id: 1,
     companyName: "SNEAKER COMPANY",
     productTitle: "fall limited edition sneakers",
     description:
       "These low-profile sneakersare your perfect casual werar companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
-    price: 125.00,
+    price: 125.0,
     discount: "50%",
-    actualPrice: 250.00
+    actualPrice: 250.0,
   },
 ];
+
+const Images = [image, image2, image3, image4];
+
+export { Item, Images };
